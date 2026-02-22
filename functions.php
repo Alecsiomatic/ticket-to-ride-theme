a9b307e77c00ffa56236f3342699bb30f5791193
longevai <alejandro@longevai.com>
2026-02-21 22:00:28 -0600

    v1.4.7: Hide page entry-title (Elementor #1128)

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Constantes del tema
-define('TTR_THEME_VERSION', '1.4.6');
+define('TTR_THEME_VERSION', '1.4.7');
 define('TTR_THEME_DIR', get_template_directory());
 define('TTR_THEME_URI', get_template_directory_uri());
 
